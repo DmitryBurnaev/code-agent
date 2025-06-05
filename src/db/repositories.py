ccb0d632b8bdbf2e1129a353fa7ddc85b210d8c0
DmitryBurnaev <dmitry.burnaev@gmail.com>
2025-06-05 10:13:09 +0300

    [#6] DB-based storage: implement UOW mechanic

--- src/db/repositories.py
+++ src/db/repositories.py
@@ -1,26 +1,20 @@
 """DB-specific module that provides specific operations on the database."""
 
 import logging
-from functools import wraps
-from types import TracebackType
 from typing import (
     Generic,
     TypeVar,
     Any,
-    Self,
     TypedDict,
     Sequence,
     Unpack,
-    Callable,
-    Awaitable,
     ParamSpec,
 )
 
 from sqlalchemy import select, BinaryExpression, delete
-from sqlalchemy.exc import NoResultFound, SQLAlchemyError
+from sqlalchemy.exc import NoResultFound
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.utils import decohints
 from src.db.models import BaseModel, Vendor
 
 ModelT = TypeVar("ModelT", bound=BaseModel)
@@ -29,39 +23,7 @@ P = ParamSpec("P")
 RT = TypeVar("RT")
 
 
-@decohints
-def transaction_commit(func: Callable[P, Awaitable[RT]]) -> Callable[P, Awaitable[RT]]:
-    """Commits changes to the DB and rollback if something went wrong."""
-
-    @wraps(func)
-    async def wrapper(*args: P.args, **kwargs: P.kwargs) -> RT:
-        self = args[0]
-        if not isinstance(self, BaseRepository):
-            raise TypeError("First argument must be BaseRepository instance")
-
-        func_details: str = f"[{self.model.__name__}]: {func.__name__}({args}, {kwargs})"
-        try:
-            logger.debug("[DB] Entering transaction block %s", func_details)
-
-            result = await func(*args, **kwargs)
-            if self.auto_flush:
-                await self.session.flush()
-
-            async with self.session.begin_nested():
-                logger.debug("[DB] Commiting %s", func_details)
-                await self.session.commit()
-
-            return result
-
-        except SQLAlchemyError as exc:
-            await self.session.rollback()
-            logger.error("[DB] Error during operation %s: %r", func_details, exc)
-            raise exc
-
-    return wrapper
-
-
-class UsersFilter(TypedDict):
+class VendorsFilter(TypedDict):
     """Simple structure to filter users by specific params"""
 
     ids: list[int] | None
@@ -74,38 +36,8 @@ class BaseRepository(Generic[ModelT]):
 
     model: type[ModelT]
 
-    def __init__(
-        self,
-        session: AsyncSession,
-        auto_flush: bool = True,
-    ) -> None:
-        self.auto_flush: bool = auto_flush
+    def __init__(self, session: AsyncSession) -> None:
         self.session: AsyncSession = session
-
-    async def __aenter__(self) -> Self:
-        return self
-
-    async def __aexit__(
-        self,
-        exc_type: type[Exception],
-        exc_val: Exception,
-        exc_tb: TracebackType | None,
-    ) -> None:
-        if not self.session:
-            logger.debug("Session already closed")
-            return
-
-        await self.session.close()
-        self.__session = None
-
-    async def close(self) -> None:
-        """Closing the current session"""
-        if self.session:
-            logger.debug("Closing session")
-            await self.session.close()
-
-        else:
-            logger.debug("Session already closed")
 
     async def get(self, instance_id: int) -> ModelT:
         """Selects instance by provided ID"""
@@ -138,7 +70,6 @@ class BaseRepository(Generic[ModelT]):
         result = await self.session.execute(statement)
         return [row[0] for row in result.fetchall()]
 
-    @transaction_commit
     async def create(self, value: dict[str, Any]) -> ModelT:
         """Creates new instance"""
         logger.debug("[DB] Creating [%s]: %s", self.model.__name__, value)
@@ -155,7 +86,6 @@ class BaseRepository(Generic[ModelT]):
 
         return instance
 
-    @transaction_commit
     async def update(self, instance: ModelT, **value: dict[str, Any]) -> None:
         """Just updates the instance with provided update_value."""
         for key, value in value.items():
@@ -163,24 +93,22 @@ class BaseRepository(Generic[ModelT]):
 
         self.session.add(instance)
 
-    @transaction_commit
     async def delete(self, instance: ModelT) -> None:
         """Remove the instance from the DB."""
         await self.session.delete(instance)
 
-    @transaction_commit
     async def delete_by_ids(self, removing_ids: Sequence[int]) -> None:
         """Remove the instances from the DB."""
         statement = delete(self.model).filter(self.model.id.in_(removing_ids))
         await self.session.execute(statement)
 
 
-class UserRepository(BaseRepository[Vendor]):
+class VendorRepository(BaseRepository[Vendor]):
     """User's repository."""
 
     model = Vendor
 
-    async def filter(self, **filters: Unpack[UsersFilter]) -> list[Vendor]:
+    async def filter(self, **filters: Unpack[VendorsFilter]) -> list[Vendor]:
         """Extra filtering users by some parameters."""
         return await self.all(**filters)
 
