ea5ed32704c7f1c1c62c9c26b6826113386a1b94
DmitryBurnaev <dmitry.burnaev@gmail.com>
2025-06-12 15:28:43 +0300

    [#6] DB-based storage: vendors settings admin

--- src/admin.py
+++ src/admin.py
@@ -2,7 +2,7 @@ from typing import Any
 
 from sqladmin import ModelView, Admin
 
-from src.db.models import User
+from src.db.models import User, Vendor, VendorSettings
 
 
 class BaseModelView(ModelView):
@@ -13,7 +13,19 @@ class UserAdmin(BaseModelView, model=User):
     column_list = [User.id, User.login, User.email, User.first_name, User.last_name]
 
 
-ADMIN_VIEWS = (UserAdmin,)
+class VendorAdmin(BaseModelView, model=Vendor):
+    column_list = [Vendor.id, Vendor.name, Vendor.slug, Vendor.is_active]
+
+
+class VendorSettingsAdmin(BaseModelView, model=VendorSettings):
+    column_list = [VendorSettings.id, VendorSettings.vendor, VendorSettings.model_prefix]
+
+
+ADMIN_VIEWS: tuple[type[BaseModelView], ...] = (
+    UserAdmin,
+    VendorAdmin,
+    VendorSettingsAdmin,
+)
 
 
 class AdminApp(Admin):
@@ -25,5 +37,6 @@ class AdminApp(Admin):
 
     def _register_views(self) -> None:
         for view in ADMIN_VIEWS:
+            print(view)
             view.admin_app = self
             self.add_view(view)
