18accb633cea28220e0c5b47915ac279b54a5a0c
DmitryBurnaev <dmitry.burnaev@gmail.com>
2025-06-27 13:11:21 +0300

    [#6] Admin Panel: token creation + details page

--- src/modules/admin/views/__init__.py
+++ src/modules/admin/views/__init__.py
@@ -1,4 +1,4 @@
 from .users import UserAdmin as UserAdmin
 from .vendors import VendorAdmin as VendorAdmin
 from .models import ModelsAdmin as ModelsAdmin
-from .tokens import TokenAdmin as TokenAdmin
+from .tokens import TokenAdminView as TokenAdmin
--- src/modules/admin/views/tokens.py
+++ src/modules/admin/views/tokens.py
@@ -1,32 +1,52 @@
 import logging
-from typing import cast, Any
+from typing import cast, Any, Tuple
 
+from sqladmin import expose
 from starlette.requests import Request
+from starlette.responses import Response
 
+from src.db.repositories import TokenRepository
+from src.db.services import SASessionUOW
 from src.modules.auth.tokens import make_token
+from src.services.cache import InMemoryCache
 from src.utils import admin_get_link
 from src.db.models import BaseModel, Token
 from src.modules.admin.views.base import BaseModelView, FormDataType
 
-__all__ = ("TokenAdmin",)
+__all__ = ("TokenAdminView",)
 logger = logging.getLogger(__name__)
 
 
-class TokenAdmin(BaseModelView, model=Token):
+class TokenAdminView(BaseModelView, model=Token):
     icon = "fa-solid fa-key"
     column_list = (Token.id, Token.user, Token.expires_at)
-    column_details_list = (
-        Token.id,
-        Token.token,
-        Token.user,
-        Token.expires_at,
-        Token.created_at,
-        Token.updated_at,
-    )
+    form_columns = (Token.name, Token.user, Token.expires_at)
+    can_edit = False
     column_formatters = {Token.id: lambda model, a: admin_get_link(cast(BaseModel, model))}
 
     async def insert_model(self, request: Request, data: FormDataType) -> Any:
         raw_token, hashed_token = make_token()
         data["token"] = hashed_token
-        # TODO: provide raw_token to the context (show user on the result admin view)
-        return await super().insert_model(request, data)
+        token: Token = await super().insert_model(request, data)
+        cache: InMemoryCache = InMemoryCache()
+        cache.set(f"token__{token.id}", raw_token)
+        return token
+
+    @expose("/tokens/:id/details", methods=["GET"])
+    async def get_models(self, id: int, request: Request) -> Response:
+        async with SASessionUOW() as uow:
+            token: Token = await TokenRepository(uow.session).get(id)
+
+        cache: InMemoryCache = InMemoryCache()
+        raw_token: str = str(cache.get(f"token__{token.id}"))
+        cache.invalidate(raw_token)
+
+        context = {
+            "token": token,
+            "raw_token": raw_token,
+        }
+        return await self.templates.TemplateResponse(
+            request,
+            name="token_details.html",
+            context=context,
+        )
