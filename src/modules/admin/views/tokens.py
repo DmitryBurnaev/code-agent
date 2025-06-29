20e49f961196ac9707d72b17fb3bd64d16d028ce
DmitryBurnaev <dmitry.burnaev@gmail.com>
2025-06-29 19:06:34 +0300

    [#6] DB-based storage: improved tokens page

--- src/modules/admin/views/tokens.py
+++ src/modules/admin/views/tokens.py
@@ -20,11 +20,16 @@ logger = logging.getLogger(__name__)
 class TokenAdminView(BaseModelView, model=Token):
     icon = "fa-solid fa-key"
     column_list = (Token.id, Token.user, Token.expires_at)
-    form_columns = (Token.name, Token.user, Token.expires_at)
+    form_columns = (Token.user, Token.name, Token.expires_at)
     can_edit = False
-    column_formatters = {Token.id: lambda model, a: admin_get_link(cast(BaseModel, model))}
+    column_formatters = {
+        Token.id: lambda model, a: admin_get_link(cast(BaseModel, model), target="details")
+    }
+    column_details_list = (Token.id, Token.user, Token.name, Token.expires_at, Token.created_at)
+    column_default_li = ()
+    details_template = "token_details.html"
 
-    async def insert_model(self, request: Request, data: FormDataType) -> Any:
+    async def insert_model(self, request: Request, data: FormDataType) -> Token:
         raw_token, hashed_token = make_token()
         data["token"] = hashed_token
         token: Token = await super().insert_model(request, data)
@@ -32,21 +37,29 @@ class TokenAdminView(BaseModelView, model=Token):
         cache.set(f"token__{token.id}", raw_token)
         return token
 
-    @expose("/tokens/:id_/details", methods=["GET"])
-    async def get_models(self, id_: int, request: Request) -> Response:
-        async with SASessionUOW() as uow:
-            token: Token = await TokenRepository(uow.session).get(id_)
-
+    async def get_object_for_details(self, value: Any) -> Token:
+        token: Token = await super().get_object_for_details(value)
         cache: InMemoryCache = InMemoryCache()
-        raw_token: str = str(cache.get(f"token__{token.id}"))
-        cache.invalidate(raw_token)
+        cache_key = f"token__{token.id}"
+        token.raw_token = str(cache.get(cache_key))
+        cache.invalidate(cache_key)
+        return token
 
-        context = {
-            "token": token,
-            "raw_token": raw_token,
-        }
-        return await self.templates.TemplateResponse(
-            request,
-            name="token_details.html",
-            context=context,
-        )
+    # @expose("/tokens/:id_/details", methods=["GET"])
+    # async def get_models(self, id_: int, request: Request) -> Response:
+    #     async with SASessionUOW() as uow:
+    #         token: Token = await TokenRepository(uow.session).get(id_)
+    #
+    #     cache: InMemoryCache = InMemoryCache()
+    #     raw_token: str = str(cache.get(f"token__{token.id}"))
+    #     cache.invalidate(raw_token)
+    #
+    #     context = {
+    #         "token": token,
+    #         "raw_token": raw_token,
+    #     }
+    #     return await self.templates.TemplateResponse(
+    #         request,
+    #         name="token_details.html",
+    #         context=context,
+    #     )
