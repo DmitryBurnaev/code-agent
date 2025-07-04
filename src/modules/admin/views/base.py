60d5862d556b1dcf81f306ccea3790ed3282062b
DmitryBurnaev <dmitry.burnaev@gmail.com>
2025-07-04 18:39:41 +0300

    [#6] DB-based storage: improve tokens generation

--- src/modules/admin/views/base.py
+++ src/modules/admin/views/base.py
@@ -1,10 +1,11 @@
 import logging
+import datetime
 from typing import ClassVar
 
 from sqladmin import ModelView
 
 logger = logging.getLogger(__name__)
-type FormDataType = dict[str, str | int | None]
+type FormDataType = dict[str, str | int | datetime.datetime | None]
 __all__ = ("BaseModelView",)
 
 
