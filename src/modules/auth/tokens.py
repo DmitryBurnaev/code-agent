a4e71a607af1a303f719efba76dc6cfb01eaa6bb
DmitryBurnaev <dmitry.burnaev@gmail.com>
2025-07-04 10:36:49 +0300

    [#6] DB-based storage: start using JWT for token's generation

--- src/modules/auth/tokens.py
+++ src/modules/auth/tokens.py
@@ -1,12 +1,15 @@
+import datetime
 import hashlib
 import uuid
 from typing import NamedTuple
 
+import jwt
 from fastapi import Security
 from fastapi.security import APIKeyHeader
 from starlette.exceptions import HTTPException
 from starlette.requests import Request
 
+from settings import get_app_settings
 from src.db.repositories import TokenRepository
 from src.db.services import SASessionUOW, logger
 
@@ -16,17 +19,45 @@ __all__ = (
     "verify_api_token",
 )
 
+from utils import utcnow
+
 
 class TokenInfo(NamedTuple):
     value: str
     hashed_value: str
 
 
-def make_token() -> TokenInfo:
+def make_token(token_expires_in: datetime.datetime | None = None) -> TokenInfo:
     """Generates token, and it hashed value (requires for storage)"""
-    token = uuid.uuid4().hex
-    hashed_token = hash_token(token)
-    return TokenInfo(value=token, hashed_value=hashed_token)
+    settings = get_app_settings()
+    user_show_id = f"{utcnow().microsecond:0>6}{uuid.uuid4().hex[-6:]}"
+    payload: dict[str, str | datetime.datetime] = {"sub": user_show_id}
+    if token_expires_in is not None:
+        payload["exp"] = token_expires_in
+
+    # TODO:
+    encrypted_token = jwt.encode(
+        payload,
+        key=settings.secret_key.get_secret_value(),
+        algorithm=settings.jwt_algorithm,
+    )
+    return TokenInfo(
+        value=encrypted_token,
+        hashed_value=hash_token(encrypted_token),
+    )
+
+
+def decode_token(token: str) -> dict:
+    settings = get_app_settings()
+    input_token: str = "user.sending.jwt-token"
+    header_part = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9"
+    t2, t3 = input_token[-43:], input_token[43:]
+    will_be_checking_token = f"{header_part}.{t2}.{t3}"
+    return jwt.decode(
+        will_be_checking_token,
+        settings.secret_key,
+        algorithms=[settings.jwt_algorithm],
+    )
 
 
 def hash_token(token: str) -> str:
